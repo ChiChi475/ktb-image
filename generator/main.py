275794a90cb7758091359463b6684e1fde55bd64
ChiChi475 <lucchi475@gmail.com>
2025-09-09 16:34:25 +0700

    Update main.py

--- generator/main.py
+++ generator/main.py
@@ -336,11 +336,11 @@ def main():
                     cleaned_title = clean_title(base_filename.replace('-', ' ').strip(), title_clean_keywords)
                     prefix = mockup_data.get("title_prefix_to_add", "")
                     suffix = mockup_data.get("title_suffix_to_add", "")
-                    final_filename = f"{prefix} {cleaned_title} {suffix}".replace('  ', ' ').strip() + '.jpg'
+                    final_filename = f"{prefix} {cleaned_title} {suffix}".replace('  ', ' ').strip() + '.webp'
                     
                     img_byte_arr = BytesIO()
-                    final_mockup_rgb = final_mockup.convert('RGB')
-                    final_mockup_rgb.save(img_byte_arr, format="JPEG", quality=90)
+                    #final_mockup_rgb = final_mockup.convert('RGB')
+                    final_mockup_rgb.save(img_byte_arr, format="WEBP", quality=90)
                     
                     if mockup_name not in images_for_zip:
                         images_for_zip[mockup_name] = []
